3afeb5b17d93791380ef85cfd14fca71a0f4bf86
Juandmj82 <juandiegosdb@gmail.com>
2025-02-26 20:55:23 -0500

    "Implementando protección del código a través del manejo de errores y excepciones"

---
+++ protecting_code_coffee/src/Latte.java
@@ -0,0 +1,22 @@
+import com.sun.jdi.InvalidTypeException;
+
+public class Latte extends Coffee {
+    String milkType;
+    String syrupFlavor;
+
+    public Latte(String name, String roast, double price, String milkType, String syrupFlavor) throws IllegalArgumentException {
+        super(name, roast, price);
+
+        if (!("whole".equals(milkType)) && !("skim".equals(milkType)) && !("almond".equals(milkType)) && !("oat".equals(milkType))) {
+            throw new IllegalArgumentException("please select a valid milk type!");
+        }
+
+        this.milkType = milkType;
+        this.syrupFlavor = syrupFlavor;
+    }
+
+    public void printLatteDetails() {
+        System.out.println("Your latte has " + milkType + " milk and " + syrupFlavor + " flavor.");
+        System.out.println("Your total bill is " + price + "$.");
+    }
+}
